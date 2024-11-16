7587074d7fef346bfb08307043c841a6e3cff87f
dimos <dimos86m@gmail.com>
2024-11-16 19:13:23 +0200

    Revert "Refactor `canEscape` parameter order for consistency"
    This reverts commit 6d9c85808a3153581526721974110ba6769ebd35.

--- src/main/scala/prisoners/main.scala
+++ src/main/scala/prisoners/main.scala
@@ -8,7 +8,7 @@ val threshold = 50
 val experiments = 10000
 
 @tailrec
-def canEscape(prisonerId: Int, boxes: Map[Int, Int], revealedNumbers: Vector[Int]): Boolean =
+def canEscape(boxes: Map[Int, Int], prisonerId: Int, revealedNumbers: Vector[Int]): Boolean =
   val nextBoxId = revealedNumbers.lastOption.getOrElse(prisonerId)
   val nextRevealedNum = boxes(nextBoxId)
   val updatedRevealedNumbers = revealedNumbers :+ nextRevealedNum
@@ -17,13 +17,13 @@ def canEscape(prisonerId: Int, boxes: Map[Int, Int], revealedNumbers: Vector[Int
   else if nextRevealedNum == prisonerId then
     true
   else
-    canEscape(prisonerId, boxes, updatedRevealedNumbers)
+    canEscape(boxes, prisonerId, updatedRevealedNumbers)
 
 def canAllEscape: Boolean =
   val ids = 1 to prisoners
   val shuffledIds = Random.shuffle(ids)
   val boxes = ids.zip(shuffledIds).toMap
-  ids.forall(prisonerId => canEscape(prisonerId, boxes, Vector()))
+  ids.forall(prisonerId => canEscape(boxes, prisonerId, Vector()))
 
 @main
 def main(): Unit =
